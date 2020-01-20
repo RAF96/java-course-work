30f68b2877efa10cb720136f392b29893cb46f31
Anton Ryabushev <ryabushev96@gmail.com>
2020-01-21 01:27:24 +0300

    commit before reinstall system, may be something wrong

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    application
     id("com.google.protobuf") version "0.8.10" apply false
 }
 
@@ -11,8 +12,14 @@ repositories {
 
 dependencies {
     testCompile("junit", "junit", "4.12")
+    implementation(project(":run"))
 }
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
+
+application {
+    mainClassName = "ru.ifmo.java.run.RunServers"
+}
+
--- common/src/main/java/ru/ifmo/java/common/Constant.java
+++ common/src/main/java/ru/ifmo/java/common/Constant.java
@@ -2,18 +2,23 @@ package ru.ifmo.java.common;
 
 import ru.ifmo.java.common.enums.ServerType;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Constant {
-    public static final String serverHost = "localhost";
+    //public static final String serverHost = "localhost";
+    public static final String serverHost = "18.216.180.203";
+
     public static final int numberThreadOfServerPool = 1;
     public static final int serverSocketTimeout = 1 * 1000;
 
-    public static final Path metricsPath = Path.of("data/metrics");
-    public static final Path metricsPathForOneClient = Path.of("data/metrics/forOneClient");
-    public static final Path runSettingsForGUI = Path.of("data/settings_file");
-    public static final Path dataPath = Path.of("data");
-    public static final Path dataSavePath = Path.of("dataSave");
+    public static final Path metricsPath = Paths.get("data/metrics");
+    public static final Path metricsPathForOneClient = Paths.get("data/metrics/forOneClient");
+    public static final Path runSettingsForGUI = Paths.get("data/settings_file");
+    public static final Path dataPath = Paths.get("data");
+    public static final Path dataSavePath = Paths.get("dataSave");
 
     public static final int individualThreadServerPort = 8081;
     public static final int blockingServerPort = 8082;
